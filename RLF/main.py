bbf7e889916eda4e35795d3c4d4b28ccb2a14c9b
lvlvlvlvlv <lv_xubo@163.com>
2017-08-20 17:52:23 +0800

    add initial 'DQN'

--- RLF/main.py
+++ RLF/main.py
@@ -8,6 +8,7 @@ from td import *
 from td_lambda import *
 
 
+
 if __name__ == '__main__':
 	
 		
@@ -44,7 +45,7 @@ if __name__ == '__main__':
 	xdata = range(0,2000,50)
 	ydata = avg_ep_return_list
 	'''
-
+	'''
 	td_lambda = Temporal_Difference_lambda(Lam_states,Lam_actions)
 	td_lambda.set_policy('naive_Q_lambda')
 	avg_ep_return_list = td_lambda.sarsa_lambda(Lamborghini, 2000, 0.1, 0.5, 1, 0.9, 200, 50)
@@ -54,6 +55,8 @@ if __name__ == '__main__':
 
 	fig = Line_Chart(xdata,ydata,"index","avg return","assessment of racetrack "+'with naive Q-lambda')
 	fig.Draw_LineChart("")
+	'''
+
 
 
 
